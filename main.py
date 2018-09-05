37d53f51d22ccad9da1188923af0295fd5e05a98
wangxiaoxiaoxian <42993353+wangxiaoxiaoxian@users.noreply.github.com>
2018-09-05 20:40:26 +0800

    Update main.py

--- main.py
+++ main.py
@@ -6,18 +6,26 @@
 # @Software: PyCharm Community Edition
 # @Dsc     : main
 import os
+import time
 file1=open('./test.txt','w')
 list1=os.listdir('./')
 print(list1)
 for i in list1:
+    ticks=time.asctime(time.localtime(time.time()))
+    print"当前时间为:",ticks
     print(i)
     file1.write(i)
     file1.write('\n')
+    file1.write(str(ticks))
+    file1.write('\n')
+
 
 file1.close()
 
 print '........'
 file2=open('./test.txt')
 lines=file2.readlines()
+length=len(lines)
+print('this file has:',str(length))
 for line in lines:
     print(line)
